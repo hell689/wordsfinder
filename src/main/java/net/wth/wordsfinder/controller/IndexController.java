21c9daaa0fdaa87834f03f458b0cdc8a887b1f0a
Alexey Vladyko <a.uladyka@gmail.com>
2020-01-23 12:16:08 +0300

    search error

--- src/main/java/net/wth/wordsfinder/controller/IndexController.java
+++ src/main/java/net/wth/wordsfinder/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -27,9 +28,16 @@ class IndexController {
     @PostMapping
     public List<String> findWords(@RequestBody Zapros zapros) {
         logger.info("Поступил запрос " + zapros);
-        List<String> words = wordsFinderService.findWords(zapros);
+        List<String> words = new ArrayList<>();
+        try {
+            words = wordsFinderService.findWords(zapros);
+        } catch (Exception e) {
+            words.add(e.getMessage());
+            e.printStackTrace();
+            return words;
+        }
         logger.info("Запрос обработан");
         return words;
     }
 
-    }
+}
